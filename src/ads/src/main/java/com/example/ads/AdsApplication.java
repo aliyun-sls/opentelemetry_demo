a02634f009748c6488f5fbe9ce4928199aa89625
李超 <wb-lc540389@alibaba-inc.com>
2025-03-15 22:01:39 +0800

    add ads

--- src/ads/src/main/java/com/example/ads/AdsApplication.java
+++ src/ads/src/main/java/com/example/ads/AdsApplication.java
@@ -2,8 +2,10 @@ package com.example.ads;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.ads")
 public class AdsApplication {
 
 	public static void main(String[] args) {
