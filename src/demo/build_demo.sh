66de51dd2a70f8ef9fac60a474bfe46380dfdc2d
李超 <wb-lc540389@alibaba-inc.com>
2025-03-17 11:33:17 +0800

    add demo

--- src/demo/build_demo.sh
+++ src/demo/build_demo.sh
@@ -5,17 +5,17 @@ DOCKER_REPO=ghcr.io/aliyun-sls/demo
 DOCKER_TAG=1.0
 
 module=ads
-sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}/${module}:${DOCKER_TAG} .
-sudo docker push ${DOCKER_REPO}/${module}:${DOCKER_TAG}
+sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module}-eks .
+sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}-eks
 
 module=notification
-sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}/${module}:${DOCKER_TAG} .
-sudo docker push ${DOCKER_REPO}/${module}:${DOCKER_TAG}
+sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module}-eks .
+sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}-eks
 
 module=promotion
-sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}/${module}:${DOCKER_TAG} .
-sudo docker push ${DOCKER_REPO}/${module}:${DOCKER_TAG}
+sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module}-eks .
+sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}-eks
 
 module=marketing
-sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}/${module}:${DOCKER_TAG} .
-sudo docker push ${DOCKER_REPO}/${module}:${DOCKER_TAG}
+sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module}-eks .
+sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}-eks
