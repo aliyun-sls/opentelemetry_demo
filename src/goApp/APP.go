4b8dea0c9cbfd22c35f69649a346de5410f9ebee
陈辉 <wb-ch916762@alibaba-inc.com>
2025-03-20 15:32:32 +0800

    test git hook

--- src/goApp/APP.go
+++ src/goApp/APP.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 type APP struct {
@@ -26,30 +27,42 @@ func main() {
 	app := &APP{}
 	fmt.Println("开启监听。。。。。")
 	http.HandleFunc("/getapp01", app.GetApp01)
-	http.ListenAndServe(":8080", nil)
+
+	// 将监听服务放在一个独立的goroutine中运行
+	go func() {
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			log.Fatalf("Error starting server: %v", err)
+		}
+	}()
 
 	url := "http://user.default.svc.cluster.local:8080/login"
 
-	// 创建请求体
-	user := User{
-		Username: "admin",
-		Password: "admin",
-	}
-	jsonData, err := json.Marshal(user)
-	if err != nil {
-		log.Fatalf("Error marshalling user data: %v", err)
-	}
+	// 创建一个每10秒触发一次的定时器
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
 
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
-	if err != nil {
-		log.Fatalf("Error calling endpoint: %v", err)
-	}
-	defer resp.Body.Close()
+	for range ticker.C {
+		// 创建请求体
+		user := User{
+			Username: "admin",
+			Password: "admin",
+		}
+		jsonData, err := json.Marshal(user)
+		if err != nil {
+			log.Fatalf("Error marshalling user data: %v", err)
+		}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Error reading response body: %v", err)
-	}
+		resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+		if err != nil {
+			log.Fatalf("Error calling endpoint: %v", err)
+		}
+		defer resp.Body.Close()
 
-	log.Printf("Response: %s", body)
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalf("Error reading response body: %v", err)
+		}
+
+		log.Printf("Response: %s", body)
+	}
 }
