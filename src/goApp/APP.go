595910ee60278b74f63823f3ef595e7308b0903c
陈辉 <wb-ch916762@alibaba-inc.com>
2025-03-20 15:09:04 +0800

    test git hook

--- src/goApp/APP.go
+++ src/goApp/APP.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -13,9 +17,39 @@ func (a *APP) GetApp01(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from /getapp01"))
 }
 
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func main() {
 	app := &APP{}
 	fmt.Println("开启监听。。。。。")
 	http.HandleFunc("/getapp01", app.GetApp01)
 	http.ListenAndServe(":8080", nil)
+
+	url := "http://user.default.svc.cluster.local:8080/login"
+
+	// 创建请求体
+	user := User{
+		Username: "admin",
+		Password: "admin",
+	}
+	jsonData, err := json.Marshal(user)
+	if err != nil {
+		log.Fatalf("Error marshalling user data: %v", err)
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Fatalf("Error calling endpoint: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading response body: %v", err)
+	}
+
+	log.Printf("Response: %s", body)
 }
