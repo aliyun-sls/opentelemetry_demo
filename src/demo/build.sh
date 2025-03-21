e5fdaccf6b80e921c3cb5a0d326b889b1af9cea1
李超 <wb-lc540389@alibaba-inc.com>
2025-03-21 16:26:35 +0800

    add gateway

--- src/demo/build.sh
+++ src/demo/build.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 
-DOCKER_REPO=o11y-demo-cn-heyuan-registry.cn-heyuan.cr.aliyuncs.com/o11y-demo-cn-heyuan
-DOCKER_TAG=1.0
+DOCKER_REPO=o11y-demo-cn-heyuan-registry.cn-heyuan.cr.aliyuncs.com/o11y-demo-cn-heyuan/demo
+DOCKER_TAG=latest
 
 module=ads
-sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}/${module}:${DOCKER_TAG} .
-sudo docker push ${DOCKER_REPO}/${module}:${DOCKER_TAG}
+sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
+sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}
 
 module=notification
-sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}/${module}:${DOCKER_TAG} .
-sudo docker push ${DOCKER_REPO}/${module}:${DOCKER_TAG}
+sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
+sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}
 
 module=promotion
-sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}/${module}:${DOCKER_TAG} .
-sudo docker push ${DOCKER_REPO}/${module}:${DOCKER_TAG}
+sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
+sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}
 
 module=marketing
-sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}/${module}:${DOCKER_TAG} .
-sudo docker push ${DOCKER_REPO}/${module}:${DOCKER_TAG}
+sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
+sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}
 
 module=gateway
-sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}/${module}:${DOCKER_TAG} .
-sudo docker push ${DOCKER_REPO}/${module}:${DOCKER_TAG}
+sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
+sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}
