0d57d99e2920c0f5b15e024e7daec99cb6f9c472
陈辉 <wb-ch916762@alibaba-inc.com>
2025-03-25 16:28:18 +0800

    fix api

--- src/demo/goApp/APP.go
+++ src/demo/goApp/APP.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	OSS "goapp/oss"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -67,7 +66,7 @@ func main() {
 			log.Fatalf("Error reading login response body: %v", err)
 		}
 		log.Printf("Login Response: %s", body)
-		OSS.PushOSS()
+		//OSS.PushOSS()
 		// GET 请求到 shelve 接口，添加查询参数
 		queryParams := "?products_cate=1&products_name=electronics&unit_price=100&products_unit=1&brand_id=123&seller_id=456"
 		respShelve, err := http.Get(urlShelve + queryParams)
