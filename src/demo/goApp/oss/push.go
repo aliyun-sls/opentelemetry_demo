d5853af286026f68a89c76eaa3232d3c65a54f02
陈辉 <wb-ch916762@alibaba-inc.com>
2025-03-25 16:43:43 +0800

    fix api

--- src/demo/goApp/oss/push.go
+++ src/demo/goApp/oss/push.go
@@ -49,7 +49,7 @@ func PushOSS() {
 func push(client *oss.Client) {
 	// 获取指定目录下的所有文件
 	path, _ := os.Getwd()
-	files, err := os.ReadDir(path + "/goApp/tupian/")
+	files, err := os.ReadDir(path + "/tupian/")
 	if err != nil {
 		log.Fatalf("failed to read directory: %v", err)
 	}
@@ -57,7 +57,7 @@ func push(client *oss.Client) {
 	// 遍历目录中的文件
 	for _, file := range files {
 		if !file.IsDir() {
-			filePath := path + "/goApp/tupian/" + file.Name()
+			filePath := path + "/tupian/" + file.Name()
 			file, err := os.Open(filePath)
 			if err != nil {
 				log.Fatalf("failed to open file: %v", err)
