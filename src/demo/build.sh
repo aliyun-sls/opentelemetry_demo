37c4e7909658c93d5def155629136d768616b101
李超 <wb-lc540389@alibaba-inc.com>
2025-03-26 17:20:27 +0800

    打印日志

--- src/demo/build.sh
+++ src/demo/build.sh
@@ -4,10 +4,6 @@
 DOCKER_REPO=o11y-demo-cn-heyuan-registry.cn-heyuan.cr.aliyuncs.com/o11y-demo-cn-heyuan/demo
 DOCKER_TAG=latest
 
-module=gateway
-sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
-sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}
-
 module=ads
 sudo docker build -f Dockerfile --no-cache --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
 sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}
@@ -22,4 +18,8 @@ sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}
 
 module=marketing
 sudo docker build -f Dockerfile --no-cache --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
+sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}
+
+module=gateway
+sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
 sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}
