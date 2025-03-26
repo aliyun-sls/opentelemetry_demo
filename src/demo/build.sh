ca9df91b557e9a1a686d966db6f1a0b18d7cf91a
李超 <wb-lc540389@alibaba-inc.com>
2025-03-26 17:19:54 +0800

    打印日志

--- src/demo/build.sh
+++ src/demo/build.sh
@@ -9,17 +9,17 @@ sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO
 sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}
 
 module=ads
-sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
+sudo docker build -f Dockerfile --no-cache --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
 sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}
 
 module=notification
-sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
+sudo docker build -f Dockerfile --no-cache --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
 sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}
 
 module=promotion
-sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
+sudo docker build -f Dockerfile --no-cache --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
 sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}
 
 module=marketing
-sudo docker build -f Dockerfile --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
+sudo docker build -f Dockerfile --no-cache --build-arg module=${module} --tag ${DOCKER_REPO}:${DOCKER_TAG}-${module} .
 sudo docker push ${DOCKER_REPO}:${DOCKER_TAG}-${module}
