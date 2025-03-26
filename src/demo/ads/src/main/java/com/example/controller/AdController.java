8af6820eab9f88767b9221cb0cd92c8770434ccc
李超 <wb-lc540389@alibaba-inc.com>
2025-03-26 16:21:18 +0800

    注入延时

--- src/demo/ads/src/main/java/com/example/controller/AdController.java
+++ src/demo/ads/src/main/java/com/example/controller/AdController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class AdController {
@@ -17,14 +19,42 @@ public class AdController {
     private AdService adService;
 
     @GetMapping("/listAds")
-    public ResponseEntity<List<AdEntity>> listAds() {
-        List<AdEntity> ads = adService.listAds();
+    public ResponseEntity<List> listAds() {
+        List ads;
+        Random random = new Random();
+        double n = random.nextDouble();
+        if (n < 0.1) {
+            ads = adService.findAdWithSleep();
+        }else {
+            if (n<0.4){
+                try {
+                    TimeUnit.SECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            ads = adService.listAds();
+        }
         return ResponseEntity.ok(ads);
     }
 
     @GetMapping("/ads/listAds")
     public ResponseEntity<List<AdEntity>> listAdsAll() {
-        List<AdEntity> ads = adService.listAds();
+        List ads = null;
+        Random random = new Random();
+        double n = random.nextDouble();
+        if (n < 0.1) {
+            ads = adService.findAdWithSleep();
+        }else {
+            if (n<0.4){
+                try {
+                    TimeUnit.SECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            ads = adService.listAds();
+        }
         return ResponseEntity.ok(ads);
     }
 
