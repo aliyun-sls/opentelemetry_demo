f2e09ec250300b96bfcaa95f18fcb2e71810f1e7
陈辉 <wb-ch916762@alibaba-inc.com>
2025-03-27 15:37:40 +0800

    fix oss

--- src/product-catalog/genproto/oteldemo/demo_grpc.pb.go
+++ src/product-catalog/genproto/oteldemo/demo_grpc.pb.go
@@ -25,8 +25,6 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
-	"math/rand"
-	"time"
 )
 
 // This is a compile-time assertion to ensure that this generated file
