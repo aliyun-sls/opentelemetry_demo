58ea45f05e03976c9ca4843ba3f167593dd889bc
陈辉 <wb-ch916762@alibaba-inc.com>
2025-04-02 16:37:57 +0800

    gocpu

--- src/demo/gocpu/cpu.go
+++ src/demo/gocpu/cpu.go
@@ -11,7 +11,7 @@ type CPU struct {
 }
 
 // ConsumeCPU 通过循环计算来消耗 CPU 资源
-func (a *CPU) ConsumeCPU() {
+func ConsumeCPU() {
 	// 通过循环计算来消耗 CPU
 	const iterations = 1000000000
 	sum := 0
@@ -35,16 +35,26 @@ func (a *CPU) cpu(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(response)
 
-	a.ConsumeCPU()
+	ConsumeCPU()
+}
+
+func run() {
+	ConsumeCPU()
 }
 
 func main() {
 	app := &CPU{}
 	fmt.Println("开启监听。。。。。")
 	http.HandleFunc("/cpu", app.cpu)
+	go func() {
+		for {
+			run()
+		}
 
+	}()
 	// 将监听服务放在一个独立的goroutine中运行
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
+
 }
