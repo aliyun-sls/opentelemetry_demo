3653758a7a7b481369d8e9d5f42874291bc63143
李超 <wb-lc540389@alibaba-inc.com>
2025-04-02 11:34:16 +0800

    add ads scenarios

--- src/demo/ads/src/main/java/com/example/entity/AdEntity.java
+++ src/demo/ads/src/main/java/com/example/entity/AdEntity.java
@@ -2,9 +2,8 @@
 
    import lombok.Getter;
    import lombok.Setter;
-   import javax.persistence.Entity;
-   import javax.persistence.Id;
-   import javax.persistence.Table;
+
+   import javax.persistence.*;
 
    @Entity
    @Getter
@@ -12,6 +11,7 @@
    @Table(name = "ads")
    public class AdEntity {
        @Id
+       @GeneratedValue(strategy = GenerationType.IDENTITY)
        private String id;
        
        private String redirectUrl;
