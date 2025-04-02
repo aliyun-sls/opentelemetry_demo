222da5838ab7a129f5b575f581f22703512f3141
陈辉 <wb-ch916762@alibaba-inc.com>
2025-04-02 16:55:53 +0800

    gocpu

--- src/demo/gocpu/cpu.go
+++ src/demo/gocpu/cpu.go
@@ -35,8 +35,6 @@ func (a *CPU) cpu(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(response)
-
-	ConsumeCPU()
 }
 
 func run() {
