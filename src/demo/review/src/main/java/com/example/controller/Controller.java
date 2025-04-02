452e4ad8ede27a6da7d2bc9919d083832333392e
李超 <wb-lc540389@alibaba-inc.com>
2025-04-02 15:45:56 +0800

    add review

--- src/demo/review/src/main/java/com/example/controller/Controller.java
+++ src/demo/review/src/main/java/com/example/controller/Controller.java
@@ -3,12 +3,25 @@ package com.example.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
     @GetMapping("/review")
     public ResponseEntity<?> review() {
-        return ResponseEntity.ok().build();
+        // 构建一个大的Map
+        Map<Integer, String> largeMap = new HashMap<>();
+        Random random = new Random();
+        for (int i = 0; i < 1000000; i++) {
+            largeMap.put(i, "value" + random.nextInt(100000));
+        }
+
+        // 在Map中查找数据
+        String value = largeMap.get(random.nextInt(1000000));
+
+        return ResponseEntity.ok(value);
     }
 }
