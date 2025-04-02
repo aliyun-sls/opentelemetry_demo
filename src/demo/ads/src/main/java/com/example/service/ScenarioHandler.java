28a0c0fe57b62708c384c94b63b6ae14fa6a3140
李超 <wb-lc540389@alibaba-inc.com>
2025-04-02 17:49:27 +0800

    add review reporting logistics

--- src/demo/ads/src/main/java/com/example/service/ScenarioHandler.java
+++ src/demo/ads/src/main/java/com/example/service/ScenarioHandler.java
@@ -20,7 +20,7 @@ public class ScenarioHandler {
     // 程序延迟
     public void handleDelay() {
         try {
-            Thread.sleep(10000);
+            Thread.sleep(3000);
             adService.listAds();
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
