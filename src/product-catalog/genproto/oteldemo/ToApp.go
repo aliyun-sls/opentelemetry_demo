9e2285093acc98761ac1bcf6a47023d9b91f4efc
陈辉 <wb-ch916762@alibaba-inc.com>
2025-04-07 11:42:51 +0800

    refuse

--- src/product-catalog/genproto/oteldemo/ToApp.go
+++ src/product-catalog/genproto/oteldemo/ToApp.go
@@ -8,15 +8,29 @@ import (
 
 func ToApp01() {
 	//service := os.Getenv("SERVICE_NAME")
-	url := "http://goapp:8080/getapp01"
+	urlgoapp := "http://goapp:8080/getapp01"
+	urlrefuse := "http://refuse:8080/getapp01"
 
-	resp, err := http.Get(url)
+	resp, err := http.Get(urlgoapp)
 	if err != nil {
 		log.Fatalf("Error calling endpoint: %v", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading response body: %v", err)
+	}
+
+	log.Printf("Response: %s", body)
+
+	resp, err = http.Get(urlrefuse)
+	if err != nil {
+		log.Fatalf("Error calling endpoint: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalf("Error reading response body: %v", err)
 	}
