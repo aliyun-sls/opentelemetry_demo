d8ef2ec4768bd84eba38ead6cf30e448874cdc2a
qiansheng91 <gavin.zx@alibaba-inc.com>
2025-04-08 16:47:37 +0800

    remove refuse app

--- src/product-catalog/genproto/oteldemo/ToApp.go
+++ src/product-catalog/genproto/oteldemo/ToApp.go
@@ -9,7 +9,7 @@ import (
 func ToApp01() {
 	//service := os.Getenv("SERVICE_NAME")
 	urlgoapp := "http://goapp:8080/getapp01"
-	urlrefuse := "http://refuse:8080/getapp01"
+	//urlrefuse := "http://refuse:8080/getapp01"
 
 	resp, err := http.Get(urlgoapp)
 	if err != nil {
@@ -23,12 +23,12 @@ func ToApp01() {
 	}
 
 	log.Printf("Response: %s", body)
-
-	resp, err = http.Get(urlrefuse)
-	if err != nil {
-		log.Fatalf("Error calling endpoint: %v", err)
-	}
-	defer resp.Body.Close()
+	//
+	//resp, err = http.Get(urlrefuse)
+	//if err != nil {
+	//	log.Fatalf("Error calling endpoint: %v", err)
+	//}
+	//defer resp.Body.Close()
 
 	body, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
