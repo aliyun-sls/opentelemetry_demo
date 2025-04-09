758939cd097c3af942fd35b610660d788a85e095
qiansheng91 <gavin.zx@alibaba-inc.com>
2025-04-09 17:48:04 +0800

    add marketingHighCpuCostLoopCountFlag

--- src/demo/marketing/src/main/java/com/example/controller/MarketingController.java
+++ src/demo/marketing/src/main/java/com/example/controller/MarketingController.java
@@ -4,12 +4,14 @@ import com.example.entity.AdEntity;
 import com.example.entity.MarketingEntity;
 import com.example.service.AdsService;
 import com.example.service.MarketingService;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
--- src/demo/marketing/src/main/java/com/example/service/MarketingServiceImpl.java
+++ src/demo/marketing/src/main/java/com/example/service/MarketingServiceImpl.java
@@ -1,19 +1,28 @@
 package com.example.service;
 
 import com.example.entity.MarketingEntity;
+import com.example.problempattern.HighCPUExecutor;
 import com.example.repository.MarketingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class MarketingServiceImpl implements MarketingService {
     @Autowired
     private MarketingRepository marketingRepository;
+
+    @Autowired
+    private HighCPUExecutor highCPUExecutor;
+
     @Override
     public List<MarketingEntity> listAds() {
-        return marketingRepository.findAll();
+        List<MarketingEntity> all = marketingRepository.findAll();
+
+        highCPUExecutor.execute();
+        return Collections.emptyList();
     }
 
     @Override
