0cbc14feafcb186a982eccb70f7590a604d8f992
Sheng <81615037+qiansheng91@users.noreply.github.com>
2025-04-10 16:47:02 +0800

    Update recommendation_server.py

--- src/recommendation/recommendation_server.py
+++ src/recommendation/recommendation_server.py
@@ -147,11 +147,11 @@ if __name__ == "__main__":
     # set_logger_provider(logger_provider)
     # log_exporter = OTLPLogExporter(insecure=True)
     # logger_provider.add_log_record_processor(BatchLogRecordProcessor(log_exporter))
-    handler = LoggingHandler(level=logging.NOTSET, logger_provider=logger_provider)
+    # handler = LoggingHandler(level=logging.NOTSET, logger_provider=logger_provider)
 
     # Attach OTLP handler to logger
-    logger = logging.getLogger('main')
-    logger.addHandler(handler)
+    # logger = logging.getLogger('main')
+    # logger.addHandler(handler)
 
     catalog_addr = must_map_env('PRODUCT_CATALOG_ADDR')
     pc_channel = grpc.insecure_channel(catalog_addr)
