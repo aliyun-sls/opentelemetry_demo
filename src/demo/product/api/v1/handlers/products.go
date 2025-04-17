19d52cae8c1d3789d0aedb74f5d0b9f857023e55
陈辉 <wb-ch916762@alibaba-inc.com>
2025-04-17 16:47:45 +0800

    chaosblade rdsloss

--- src/demo/product/api/v1/handlers/products.go
+++ src/demo/product/api/v1/handlers/products.go
@@ -166,6 +166,8 @@ func PutProducts(c *gin.Context) {
 	if isSwitch == "on" && lastconfig != isSwitch {
 		RDS()
 		lastconfig = isSwitch
+	} else if isSwitch == "off" {
+		lastconfig = ""
 	}
 
 	// 处理文件上传
