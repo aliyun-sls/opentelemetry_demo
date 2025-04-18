6c55d0ab0a32148c2c3b2f10d114eab2ac805dd8
陈辉 <wb-ch916762@alibaba-inc.com>
2025-04-18 14:59:44 +0800

    chaosblade nodeloss and regionloss

--- src/demo/product/api/v1/handlers/products.go
+++ src/demo/product/api/v1/handlers/products.go
@@ -202,7 +202,7 @@ func PutProducts(c *gin.Context) {
 
 	// 检查是否已存在相同记录
 	err = util.MDB.WithContext(ctx).Where("brand_id = ? AND seller_id = ?", products.BrandId, products.SellerId).First(&products).Error
-	if err == nil {
+	if err != nil {
 		if err := util.MDB.WithContext(ctx).Create(&products).Error; err != nil {
 			util.Status500(c, err)
 		}
