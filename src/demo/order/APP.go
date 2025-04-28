5b0447ca7ec82439ae52194490388a8a5e7ea51e
陈辉 <wb-ch916762@alibaba-inc.com>
2025-04-28 14:59:04 +0800

    service error

--- src/demo/order/APP.go
+++ src/demo/order/APP.go
@@ -64,7 +64,7 @@ func main() {
 }
 
 func service() {
-	response, err := http.Get("http://test:8080")
+	response, err := http.Get("http://abnormal:8080")
 	if err != nil {
 		log.Printf("Error calling shelve endpoint: %v", err)
 	}
