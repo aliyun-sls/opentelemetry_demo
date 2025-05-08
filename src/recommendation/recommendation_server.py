f6ca268a60e69242e8dae31529d8fdd51f9801e1
qiansheng91 <gavin.zx@alibaba-inc.com>
2025-05-08 14:04:28 +0800

    refine code

--- src/recommendation/recommendation_server.py
+++ src/recommendation/recommendation_server.py
@@ -97,6 +97,8 @@ def get_product_list(request_product_ids):
         # 获取所有商品信息
         cat_response = product_catalog_stub.ListProducts(demo_pb2.Empty())
         all_products = cat_response.products
+
+        logger.info(f"所有商品信息: {all_products}")
         
         # 获取请求的商品详情
         requested_products = []
