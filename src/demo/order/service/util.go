d008aba94011be9dedcdf8b82f6447ff2abfb639
任奋财 <wb-rfc754217@alibaba-inc.com>
2025-05-16 14:25:39 +0800

    update cert url

--- src/demo/order/service/util.go
+++ src/demo/order/service/util.go
@@ -100,7 +100,7 @@ func DoInitConsumer(cfg *KafkaConfig) *kafka.Consumer {
 	case "sasl_ssl":
 		kafkaconf.SetKey("security.protocol", "sasl_ssl")
 		//根据证书正式路径替换 /Users/victor/rfcwork/work/opentelemetry_demo/src/demo/order/config/ca-cert.pem
-		kafkaconf.SetKey("ssl.ca.location", "/usr/src/app/config/ca-cert.pem")
+		kafkaconf.SetKey("ssl.ca.location", "/app/config/ca-cert.pem")
 		kafkaconf.SetKey("sasl.username", cfg.SaslUsername)
 		kafkaconf.SetKey("sasl.password", cfg.SaslPassword)
 		kafkaconf.SetKey("sasl.mechanism", cfg.SaslMechanism)
