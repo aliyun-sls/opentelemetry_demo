744fc14c39f7c98f83448ee11a4d5bbb5cbd1e01
qiansheng91 <gavin.zx@alibaba-inc.com>
2025-05-22 21:15:11 +0800

    refine code

--- src/checkout/main.go
+++ src/checkout/main.go
@@ -8,8 +8,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"google.golang.org/grpc/metadata"
-	"google.golang.org/protobuf/runtime/protoimpl"
 	"io/ioutil"
 	"net"
 	"net/http"
@@ -17,6 +15,11 @@ import (
 	"strconv"
 	"sync"
 	"time"
+
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/runtime/protoimpl"
+
+	otelcodes "go.opentelemetry.io/otel/codes"
 
 	"go.opentelemetry.io/otel/attribute"
 	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
@@ -645,11 +648,11 @@ func (cs *checkout) sendToPostProcessor(ctx context.Context, result *pb.OrderRes
 	}
 
 	// Inject tracing info into message
-	//span := createProducerSpan(ctx, &msg)
-	//defer span.End()
+	span := createProducerSpan(ctx, &msg)
+	defer span.End()
 
 	// Send message and handle response
-	/*startTime := time.Now()
+	startTime := time.Now()
 	select {
 	case cs.KafkaProducerClient.Input() <- &msg:
 		log.Infof("Message sent to Kafka: %v", msg)
@@ -684,7 +687,7 @@ func (cs *checkout) sendToPostProcessor(ctx context.Context, result *pb.OrderRes
 		span.SetStatus(otelcodes.Error, "Failed to send: "+ctx.Err().Error())
 		log.Errorf("Failed to send message to Kafka within context deadline: %v", ctx.Err())
 		return
-	}*/
+	}
 
 	ffValue := cs.getIntFeatureFlag(ctx, "kafkaQueueProblems")
 	if ffValue > 0 {
