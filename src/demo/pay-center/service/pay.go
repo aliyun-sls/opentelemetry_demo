80cf496a4bec123b1ae0f8f949cb9d634411516e
任奋财 <wb-rfc754217@alibaba-inc.com>
2025-05-22 15:29:11 +0800

    update

--- src/demo/pay-center/service/pay.go
+++ src/demo/pay-center/service/pay.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	flagd "github.com/open-feature/go-sdk-contrib/providers/flagd/pkg"
 	"github.com/open-feature/go-sdk/openfeature"
@@ -85,7 +86,7 @@ func (Order) TableName() string {
 }
 
 func PayOrder(c *gin.Context) {
-	ctx := c.Request.Context()
+	ctx := context.Background()
 	var order Order
 	err := c.BindJSON(&order)
 	if err != nil {
@@ -94,24 +95,11 @@ func PayOrder(c *gin.Context) {
 	}
 
 	//todo 故障注入
-	db := util.MDB
-	tx := db.Begin()
-	defer func() {
-		if r := recover(); r != nil {
-			tx.Rollback()
-		}
-	}()
-	tx1 := tx.WithContext(ctx).Model(&order).
+	tx := util.MDB.WithContext(ctx).Model(&order).
 		Where("user_id = ? AND order_id = ?", order.UserId, order.OrderId).Update("order_status", WaitForSending)
 
-	if tx1.Error != nil {
-		tx.Rollback()
-		util.Status500(c, tx1.Error)
-		return
-	}
-	// 提交事务
-	if err := tx.Commit().Error; err != nil {
-		util.Status500(c, err)
+	if tx.Error != nil {
+		util.Status500(c, tx.Error)
 		return
 	}
 
