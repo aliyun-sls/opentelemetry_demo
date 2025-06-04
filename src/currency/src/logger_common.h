4180608ab39005e5215fb6ef5dc116b7d9ced5fe
qiansheng91 <gavin.zx@alibaba-inc.com>
2025-06-04 15:19:23 +0800

    refine currency code

--- src/currency/src/logger_common.h
+++ src/currency/src/logger_common.h
@@ -1,13 +1,13 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-#include "opentelemetry/exporters/otlp/otlp_grpc_exporter_factory.h"
+#include "opentelemetry/exporters/otlp/otlp_http_exporter_factory.h"
 #include "opentelemetry/logs/provider.h"
 #include "opentelemetry/sdk/logs/logger.h"
 #include "opentelemetry/sdk/logs/logger_provider_factory.h"
 #include "opentelemetry/sdk/logs/simple_log_record_processor_factory.h"
 #include "opentelemetry/sdk/logs/logger_context_factory.h"
-#include "opentelemetry/exporters/otlp/otlp_grpc_log_record_exporter_factory.h"
+#include "opentelemetry/exporters/otlp/otlp_http_log_record_exporter_factory.h"
 
 using namespace std;
 namespace nostd     = opentelemetry::nostd;
@@ -18,8 +18,8 @@ namespace logs_sdk  = opentelemetry::sdk::logs;
 namespace
 {
   void initLogger() {
-    otlp::OtlpGrpcLogRecordExporterOptions loggerOptions;
-    auto exporter  = otlp::OtlpGrpcLogRecordExporterFactory::Create(loggerOptions);
+    otlp::OtlpHttpLogRecordExporterOptions loggerOptions;
+    auto exporter  = otlp::OtlpHttpLogRecordExporterFactory::Create(loggerOptions);
     auto processor = logs_sdk::SimpleLogRecordProcessorFactory::Create(std::move(exporter));
     std::vector<std::unique_ptr<logs_sdk::LogRecordProcessor>> processors;
     processors.push_back(std::move(processor));
